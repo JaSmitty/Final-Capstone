9d78379db8d5a25f6a9d156756a6cffec75841ac
Jason Simon <jsimon1488@gmail.com>
2020-08-03 11:52:01 -0400

    Non-changes

---
+++ vue/src/views/User.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <games-list />
+      <!-- add a button to create a game -->
+      <router-link>
+          <button>Create a new game</button>
+    </router-link>
+      
+  </div>
+</template>
+
+<script>
+import GamesList from '@/components/GamesList.vue'
+export default {
+    GamesList
+}
+</script>
+
+<style>
+
+</style>
