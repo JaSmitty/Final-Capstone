1b4a15c871cf2b543fd009f13fdd9ef133973a27
Joshua Smith <jasbuckmaster@gmail.com>
2020-08-03 11:50:25 -0400

    DAO Work

--- dotnet/Capstone/DAO/IGameDAO.cs
+++ dotnet/Capstone/DAO/IGameDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capstone.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,9 +9,7 @@ namespace Capstone.DAO
     public interface IGameDAO
     {
         Game GetGameByUserId(int userId);
-
         Game CreateGame(int creatorId, string gameName, DateTime endDate);
-
         UserGame AddUserToGame(int addedUserId, int gameId);
 
 
