e6d5660332d64a627133b4637f7859ece73df50e
Jason Simon <jsimon1488@gmail.com>
2020-08-04 10:01:47 -0400

    write checkbox table for InviteFriends

---
+++ vue/src/components/InviteFriends.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <table>
+      <tr>
+        <td>
+          <input
+            type="checkbox"
+            id="selectAll"
+            v-bind:checked="isSelectAll"
+            v-on:change="selectAll"
+          />
+        </td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td>
+          <input
+            type="checkbox"
+            v-bind:id="user.id"
+            v-bind:value="user.id"
+            v-model="selectedUserIDs"
+          />
+        </td>
+        <td>test</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style>
+</style>
