dfa6afb07e3314eda2bffa231fb6d0515f5c0d33
Jason Simon <jsimon1488@gmail.com>
2020-08-04 09:53:12 -0400

    Add InviteFriends component

--- vue/src/views/User.vue
+++ vue/src/views/User.vue
@@ -3,7 +3,7 @@
       <games-list />
 
       <create-game />
-      
+      <invite-friends />
       
   </div>
 </template>
@@ -11,11 +11,12 @@
 <script>
 import GamesList from '@/components/GamesList.vue'
 import CreateGame from '@/components/CreateGame.vue'
+import InviteFriends from '@/components/InviteFriends.vue'
 export default {
     components: {
         GamesList,
-        CreateGame
-
+        CreateGame,
+        InviteFriends
     }
 }
 </script>
