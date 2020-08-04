f74feb8cdd70c0f19828b1ebedf8e6879929076a
Joshua Smith <jasbuckmaster@gmail.com>
2020-08-04 09:58:57 -0400

    User Controller completed

---
+++ dotnet/Capstone/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone.DAO;
+using Capstone.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone.Controllers
+{
+    [Route("users/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserSqlDAO UserDAO;
+
+        public UserController(UserSqlDAO userDAO)
+        {
+            this.UserDAO = userDAO;
+        }
+
+        [HttpGet]
+        [Route("{userId}")]
+        public ActionResult<List<UserInfo>> GetAllOtherUsers(int userId)
+        {
+            try
+            {
+                return Ok(UserDAO.GetAllOtherUsers(userId));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
