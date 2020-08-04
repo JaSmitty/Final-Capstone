9333b3744284781d4aafe9944e1c2428a7ebbeb2
Jason Simon <jsimon1488@gmail.com>
2020-08-04 08:49:14 -0400

    Fix database

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -29,7 +29,7 @@ Create table game (
 	organizer_id int not null,
 	name varchar(100) not null,
 	-- should we have a begin date?
-	end_date DateTime not null,
+	end_date Date not null,
 	Constraint pk_game_id Primary Key (id),
 	Constraint fk_game_organizer_users_id Foreign Key (organizer_id) References users(id)
 );
@@ -38,7 +38,7 @@ Create table users_game (
 	users_id int not null,
 	game_id int not null,
 	balance money not null,
-	--Constraint pk_users_id_game_id Primary Key (users_id, game_id),
+	Constraint pk_users_id_game_id Primary Key (users_id, game_id),
 	Constraint fk_users_game_users_id_users_id Foreign Key (users_id) References users(id),
 	Constraint fk_users_game_game_id_game_id Foreign Key (game_id) References game(id)
 );
@@ -70,8 +70,8 @@ Create table investment (
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
 
-INSERT INTO game (organizer_id, name, end_date) VALUES (1, 'game1', 2020-08-10);
-INSERT INTO game (organizer_id, name, end_date) VALUES (2, 'game2', 2020-08-08);
+INSERT INTO game (organizer_id, name, end_date) VALUES (1, 'game1', '2020-08-10');
+INSERT INTO game (organizer_id, name, end_date) VALUES (2, 'game2', '2020-08-08');
 
 INSERT INTO company(ticker, open_price, high_price, low_price, current_price, previous_close_price) VALUES ('AAPL', 432.80, 446.55, 431.57, 437.70, 425.04);
 INSERT INTO company(ticker, open_price, high_price, low_price, current_price, previous_close_price) VALUES ('PGR', 90.70, 91.23, 90.15, 90.99, 90.34);
