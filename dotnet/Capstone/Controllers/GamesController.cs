00b021eae802e48755fcdc9ecc9e1565966befd8
Joe LoCicero <jlocicero83@gmail.com>
2020-08-05 09:10:08 -0400

    GamesController method add users to game

--- dotnet/Capstone/Controllers/GamesController.cs
+++ dotnet/Capstone/Controllers/GamesController.cs
@@ -48,6 +48,21 @@ namespace Capstone.Controllers
             }
         }
         // TODO Write a controller method to receive a user ID and game ID to add to the users_game table
+        // {userId}/ invite
 
+        [HttpPost]
+        [Route("{userId}/invite")]
+        public ActionResult<bool> AddUsersToGame(List<UserGame> userGames, int userId)
+        {
+            try
+            {
+                string location = $"api/games/{userId}/invite";
+                return Created(location, gameSqlDAO.AddUserToGame(userGames));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
     }
 }
