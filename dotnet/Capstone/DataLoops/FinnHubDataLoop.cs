0cc49edaff4e947442a5886514df9a1231e38b00
Joshua Smith <jasbuckmaster@gmail.com>
2020-08-06 08:51:59 -0400

    Data loop work in progress

---
+++ dotnet/Capstone/DataLoops/FinnHubDataLoop.cs
@@ -0,0 +1,31 @@
+﻿using Capstone.API;
+using Capstone.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Capstone.DataLoops
+{
+    public class FinnHubDataLoop
+    {
+        StockAPI stockAPI = new StockAPI();
+        private readonly CompanySqlDAO companySqlDAO;
+        public FinnHubDataLoop(CompanySqlDAO companySql)
+        {
+            this.companySqlDAO = companySql;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                companySqlDAO.AddStock(stockAPI.GetCompanyStockInfo("AAPL"));
+                Thread.Sleep(60000);
+            }
+        }
+
+        
+    }
+}
