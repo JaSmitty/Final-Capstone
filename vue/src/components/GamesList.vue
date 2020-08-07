f6662dd2c941133e3bb1eb49903862c418d97630
Joe LoCicero <jlocicero83@gmail.com>
2020-08-07 12:48:38 -0400

    more styling

--- vue/src/components/GamesList.vue
+++ vue/src/components/GamesList.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="games-list">
+  <div id="games-list">
     <div class="active">
     <h1 class="title">Active Games</h1>
-    <div class="game-card-display">
+    <div class="game-card-display open">
       <active-game-details id="game" v-for="game in activeGames" :key="game.gameID" :game="game" />
     </div>
     </div>
-    <div class="invites">
+    <div class="invitations">
     <h1 class="title">Invitations</h1>
-    <div class="game-card-display">
+    <div class="game-card-display invited">
       <pending-game-details id="game" v-for="game in pendingGames" :key="game.gameID" :game="game" />
     </div>
     </div>
@@ -49,33 +49,48 @@ export default {
 
 <style scoped>
 
-.games-list{
+#games-list{
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-template-areas: "active invites";
 }
 .active {
   grid-area: active;
-  border-right: 2px solid #e7f3ff;
+  border-right: 3px solid #e7f3ff;
 }
 
 .invites{
   grid-area: invites;
-  border-left: 2px solid #e7f3ff;
+  border-left: 3px solid #e7f3ff;
 }
 
 #game {
-  border-left: solid #0059b3 7px;
+  border-left: solid #005cb8 7px;
   padding-left: 15px;
   padding-bottom: 5px;
   padding-top: 1px;
   margin-bottom: 25px;
-  background: linear-gradient(to right, #66b3ff, #cce6ff);
+  background: linear-gradient(to right, #5cadff,
+#e7f3ff);
   border-radius: 8px;
   width: 50%;
-}
+  }
+
+  #game:hover{
+    border-left: solid #f6710b 7px;
+  padding-left: 15px;
+  padding-bottom: 5px;
+  padding-top: 1px;
+  margin-bottom: 25px;
+  background: linear-gradient(to right, #fbbf92,
+#feeadb);
+  border-radius: 8px;
+  width: 50%;
+  }
+
+  
 .title {
-  background: #0080ff;
+  background: #005cb8;
   color: white;
   padding-left: 15px;
   padding-top: 20px;
@@ -86,5 +101,7 @@ export default {
 
 .game-card-display {
   display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
