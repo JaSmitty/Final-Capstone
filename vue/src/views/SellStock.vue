cd3dc22a4c3d3c1b26f1af707c20c0ff95e5d616
Jason Simon <jsimon1488@gmail.com>
2020-08-11 14:09:22 -0400

    Add completed and not yet started headers

--- vue/src/views/SellStock.vue
+++ vue/src/views/SellStock.vue
@@ -1,38 +1,40 @@
 <template>
   <div>
+    <router-link :to="{name: 'Game', params: {gameId: $route.params.gameId}}">Back To Game</router-link>
     <stock-details :stock="stock" />
     <div class="sell">
       <div class="sell-card">
-    <h2>Current shares: {{investment.sharesCurrentlyOwned}}</h2>
-    <h2>Purchase price per share: ${{investment.amountPerShare}}</h2>
-    <h2>Current price per share: ${{stock.c}}</h2>
-    <h2>
-      Per share {{profitPerShare >= 0 ? 'profit: ' : 'loss: '}}
-      <span
-        :class="profitPerShare >= 0 ? 'green' : 'red'"
-      >${{Math.abs(profitPerShare)}}</span>
-    </h2>
-    <form @submit.prevent="submitSell">
-      <h2>
-        <label for="sharesToSell">Enter shares to liquidate:</label>
-        <input
-          type="number"
-          id="sharesToSell"
-          min="0"
-          :max="investment.sharesCurrentlyOwned"
-          required
-          v-model.number="investmentToSell.sharesSold"
-        />
-      </h2>
-      <h2>
-        Total {{profitPerShare >= 0 ? 'Profit: ' : 'Loss: '}}
-        <span
-          :class="profitPerShare >= 0 ? 'green' : 'red'"
-        >${{Math.abs(totalProfit)}}</span>
-      </h2>
-      <input class="btn" type="submit" value="Sell" />
-    </form>
-    </div>
+        <h2>Current shares: {{investment.sharesCurrentlyOwned}}</h2>
+        <h2>Purchase price per share: ${{investment.amountPerShare}}</h2>
+        <h2>Current price per share: ${{stock.c}}</h2>
+        <h2>
+          Per share {{profitPerShare >= 0 ? 'profit: ' : 'loss: '}}
+          <span
+            :class="profitPerShare >= 0 ? 'green' : 'red'"
+          >${{Math.abs(profitPerShare)}}</span>
+        </h2>
+        <form @submit.prevent="submitSell">
+          <h2>
+            <label for="sharesToSell">Enter shares to liquidate:</label>
+            <input
+              type="number"
+              id="sharesToSell"
+              min="0"
+              :max="investment.sharesCurrentlyOwned"
+              step="0.01"
+              required
+              v-model.number="investmentToSell.sharesSold"
+            />
+          </h2>
+          <h2>
+            Total {{profitPerShare >= 0 ? 'Profit: ' : 'Loss: '}}
+            <span
+              :class="profitPerShare >= 0 ? 'green' : 'red'"
+            >${{Math.abs(totalProfit)}}</span>
+          </h2>
+          <input class="btn" type="submit" value="Sell" />
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -67,21 +69,25 @@ export default {
     },
   },
   methods: {
-      submitSell() {
-          stocksService.submitSell(this.investmentToSell);
-      }
-  }
+    submitSell() {
+      stocksService.submitSell(this.investmentToSell).then((response) => {
+        if (response.status === 201) {
+          this.$router.push(`/games/${this.$route.params.gameId}`);
+        }
+      });
+    },
+  },
 };
 </script>
 
 <style>
-.sell{
-  display:flex;
+.sell {
+  display: flex;
   justify-content: center;
 }
 
-.sell-card{
-    background: radial-gradient(#fcd5b6, 	#f06e04);
+.sell-card {
+  background: radial-gradient(#fcd5b6, #f06e04);
   border-radius: 7px;
   width: 750px;
   margin-top: 70px;
